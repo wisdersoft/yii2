85a1755e9acb3b618c94c98603485a91047eb119
Dickens Gao <DickensGao@protonmail.ch>
2019-11-28 21:32:25 +0800

    Add base rest Controller.

---
+++ src/rest/Controller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace wisder\yii\rest;
+
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpHeaderAuth;
+use yii\filters\auth\QueryParamAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+class Controller extends \yii\rest\Controller
+{
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::class,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
+            'verbFilter' => [
+                'class' => VerbFilter::class,
+                'actions' => $this->verbs(),
+            ],
+            'authenticator' => [
+                'class' => CompositeAuth::class,
+                'authMethods' => [
+                    [
+                        'class' => HttpHeaderAuth::class,
+                        'header' => 'token',
+                    ],
+                    [
+                        'class' => QueryParamAuth::class,
+                        'tokenParam' => 'token',
+                    ],
+                ],
+            ],
+            'rateLimiter' => [
+                'class' => RateLimiter::class,
+            ],
+        ];
+    }
+}
